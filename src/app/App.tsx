1f8f4825eca61cffe803ae02b9285526cba829c0
Aleksandr Abramov <aleksandr.a@findbride.com>
2023-09-27 03:04:21 +0600

    012 Navbar

--- src/app/App.tsx
+++ src/app/App.tsx
@@ -1,8 +1,8 @@
-import {Link} from "react-router-dom";
 import {classNames} from "shared/lib/classNames/classNames";
+import {AppRouter} from "app/providers/router";
+import {Navbar} from "widgets/Navbar";
 
 import {useTheme} from "./providers/ThemeProvider";
-import {AppRouter} from "app/providers/router";
 import './styles/index.scss';
 
 
@@ -11,8 +11,7 @@ export const App = () => {
 
     return (
         <div className={classNames("app", {}, [theme])}>
-            <Link to='/'>Главная</Link>
-            <Link to='/about'>О сайте</Link>
+            <Navbar/>
             <AppRouter/>
             <button onClick={toggleTheme}>Change theme</button>
         </div>
--- src/shared/lib/classNames/classNames.ts
+++ src/shared/lib/classNames/classNames.ts
@@ -1,8 +1,9 @@
 type Mods = Record<string, string | boolean>
 
-export function classNames(cls: string, mods: Mods, additional: string[]): string {
+export function classNames(cls: string, mods: Mods = {}, additional: string[] = []): string {
     return [cls]
         .concat(additional)
+        .filter(Boolean)
         .concat(Object.entries(mods)
             .filter(([, value]) => Boolean(value))
             .map(([className]) => className))
