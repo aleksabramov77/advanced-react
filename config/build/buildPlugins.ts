355ab866c6ef5d99ba9c755054a19b1abbbadcd7
Aleksandr Abramov <aleksandr.a@findbride.com>
2023-09-29 03:09:11 +0600

    016 Hot refresh webpack

--- config/build/buildPlugins.ts
+++ config/build/buildPlugins.ts
@@ -1,21 +1,25 @@
 import HtmlWebpackPlugin from "html-webpack-plugin";
-import {DefinePlugin, ProgressPlugin, WebpackPluginInstance} from "webpack";
+import webpack from "webpack";
 import {BuildOptions} from "./types/config";
 import MiniCssExtractPlugin from "mini-css-extract-plugin";
+import ReactRefreshWebpackPlugin from "@pmmmwh/react-refresh-webpack-plugin";
 
-export function buildPlugins({paths, isDev}: BuildOptions): WebpackPluginInstance[] {
+export function buildPlugins({paths, isDev}: BuildOptions): webpack.WebpackPluginInstance[] {
 
     return [
         new HtmlWebpackPlugin({
             template: paths.html
         }),
-        new ProgressPlugin(),
+        new webpack.ProgressPlugin(),
         new MiniCssExtractPlugin({
             filename: 'css/[name].[contenthash:8].css',
             chunkFilename: 'css/[name].[contenthash:8].css',
         }),
-        new DefinePlugin({
+        new webpack.DefinePlugin({
             __IS_DEV__: JSON.stringify(isDev)
-        })
+        }),
+        isDev ?
+            new ReactRefreshWebpackPlugin() :
+            new webpack.HotModuleReplacementPlugin()
     ]
 }
