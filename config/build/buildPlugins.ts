53baab8f97d5cc062205551df262e91816ff9fe7
Aleksandr Abramov <aleksandr.a@findbride.com>
2023-10-25 13:02:16 +0600

    023 Bundle analyser

--- config/build/buildPlugins.ts
+++ config/build/buildPlugins.ts
@@ -2,6 +2,7 @@ import HtmlWebpackPlugin from 'html-webpack-plugin';
 import webpack from 'webpack';
 import MiniCssExtractPlugin from 'mini-css-extract-plugin';
 import ReactRefreshWebpackPlugin from '@pmmmwh/react-refresh-webpack-plugin';
+import { BundleAnalyzerPlugin } from 'webpack-bundle-analyzer';
 import { BuildOptions } from './types/config';
 
 export function buildPlugins({ paths, isDev }: BuildOptions): webpack.WebpackPluginInstance[] {
@@ -20,5 +21,8 @@ export function buildPlugins({ paths, isDev }: BuildOptions): webpack.WebpackPlu
         isDev
             ? new ReactRefreshWebpackPlugin()
             : new webpack.HotModuleReplacementPlugin(),
+        new BundleAnalyzerPlugin({
+            openAnalyzer: false,
+        }),
     ];
 }
