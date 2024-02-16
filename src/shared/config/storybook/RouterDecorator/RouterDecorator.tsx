084e09e19ea80e6b169770ff51b7b7205cf3a3e8
Aleksandr Abramov <aleksandr.a@findbride.com>
2024-02-17 03:27:30 +0600

    051 Comments. Нормализация

--- src/shared/config/storybook/RouterDecorator/RouterDecorator.tsx
+++ src/shared/config/storybook/RouterDecorator/RouterDecorator.tsx
@@ -1,8 +1,8 @@
-import { Decorator } from '@storybook/react';
+import { Story } from '@storybook/react';
 import { BrowserRouter } from 'react-router-dom';
 
-export const RouterDecorator: Decorator = (Story) => (
+export const RouterDecorator = (story: () => Story) => (
     <BrowserRouter>
-        <Story />
+        {story()}
     </BrowserRouter>
 );
--- src/shared/config/storybook/ThemeDecorator/ThemeDecorator.tsx
+++ src/shared/config/storybook/ThemeDecorator/ThemeDecorator.tsx
@@ -1,10 +1,10 @@
-import { Decorator } from '@storybook/react';
+import { Story } from '@storybook/react';
 import { Theme, ThemeProvider } from 'app/providers/ThemeProvider';
 
-export const ThemeDecorator = (theme: Theme): Decorator => (Story) => (
+export const ThemeDecorator = (theme: Theme) => (StoryComponent: Story) => (
     <ThemeProvider initialTheme={theme}>
         <div className={`app ${theme}`}>
-            <Story />
+            <StoryComponent />
         </div>
     </ThemeProvider>
 );
