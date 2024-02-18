41a9b4894bb1ba919aeec511c21428a72f61e038
Aleksandr Abramov <aleksandr.a@findbride.com>
2024-02-18 16:52:20 +0600

    052 sendComment Normalize

--- src/app/providers/StoreProvider/config/StateSchema.ts
+++ src/app/providers/StoreProvider/config/StateSchema.ts
@@ -10,6 +10,7 @@ import { AxiosInstance } from 'axios';
 import { NavigateFunction } from 'react-router-dom';
 import { ArticleDetailsSchema } from 'entities/Article';
 import { ArticleDetailsCommentsSchema } from 'pages/ArticleDetailsPage';
+import { AddCommentFormSchema } from 'features/addCommentForm';
 
 export interface StateSchema {
     counter: CounterSchema;
@@ -20,14 +21,18 @@ export interface StateSchema {
     profile?: ProfileSchema;
     articleDetails?: ArticleDetailsSchema;
     articleDetailsComments?: ArticleDetailsCommentsSchema;
+    addCommentForm?: AddCommentFormSchema,
 }
 
 export type StateSchemaKey = keyof StateSchema;
 
 export interface ReducerManager {
     getReducerMap(): ReducersMapObject<StateSchema>;
+
     reduce(state: StateSchema, action: AnyAction): CombinedState<StateSchema>;
+
     add(key: StateSchemaKey, reducer: Reducer): void;
+
     remove(key: StateSchemaKey): void;
 }
 
