75903914ca0f1e0c3087e6e0d16814588556e182
Aleksandr Abramov <aleksandr.a@findbride.com>
2024-04-01 03:20:02 +0600

    055 ArticlesList slice

--- src/app/providers/StoreProvider/config/StateSchema.ts
+++ src/app/providers/StoreProvider/config/StateSchema.ts
@@ -11,6 +11,7 @@ import { NavigateFunction } from 'react-router-dom';
 import { ArticleDetailsSchema } from 'entities/Article';
 import { ArticleDetailsCommentsSchema } from 'pages/ArticleDetailsPage';
 import { AddCommentFormSchema } from 'features/addCommentForm';
+import { ArticlesPageSchema } from 'pages/ArticlesPage';
 
 export interface StateSchema {
     counter: CounterSchema;
@@ -21,26 +22,24 @@ export interface StateSchema {
     profile?: ProfileSchema;
     articleDetails?: ArticleDetailsSchema;
     articleDetailsComments?: ArticleDetailsCommentsSchema;
-    addCommentForm?: AddCommentFormSchema,
+    addCommentForm?: AddCommentFormSchema;
+    articlesPage?: ArticlesPageSchema;
 }
 
 export type StateSchemaKey = keyof StateSchema;
 
 export interface ReducerManager {
-    getReducerMap(): ReducersMapObject<StateSchema>;
-
-    reduce(state: StateSchema, action: AnyAction): CombinedState<StateSchema>;
-
-    add(key: StateSchemaKey, reducer: Reducer): void;
-
-    remove(key: StateSchemaKey): void;
+    getReducerMap: () => ReducersMapObject<StateSchema>;
+    reduce: (state: StateSchema, action: AnyAction) => CombinedState<StateSchema>;
+    add: (key: StateSchemaKey, reducer: Reducer) => void;
+    remove: (key: StateSchemaKey) => void;
 }
 
 export interface ReduxStoreWithManager extends EnhancedStore<StateSchema> {
     reducerManager: ReducerManager;
 }
 
-interface ThunkExtraArg {
+export interface ThunkExtraArg {
     api: AxiosInstance;
     navigate?: NavigateFunction
 }
